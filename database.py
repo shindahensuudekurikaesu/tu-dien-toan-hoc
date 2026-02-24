625155ad726fe347bd21a4d2e270e39e619d4a6d
shindahensuudekurikaesu <nduyanh2508@gmail.com>
2026-02-25 00:08:10 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-chu_de = {
+tu_dien_chu_de = {
     'Mệnh đề và Tập hợp': {   'Mệnh đề toán học': {   'định nghĩa': 'Là một câu khẳng định có tính đúng hoặc sai rõ '
                                                                     'ràng. Một mệnh đề không thể vừa đúng vừa sai.',
                                                       'kí hiệu': 'P, Q, R',
